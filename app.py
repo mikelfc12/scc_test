cc68d63c66192ba8a425a64723ef9dd73a701e14
Michael Dixon <michael.dixon@4-most.co.uk>
2026-03-09 14:33:00 +0000

    added data to test

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+data_test = pd.read_csv("test_data_conn.csv")
+
 st.title("Streamlit Deployment Test")
 
 st.write("If you can see this, your GitHub → Streamlit Cloud connection works!")
@@ -22,3 +24,4 @@ st.line_chart(data)
 # button
 if st.button("Click me"):
     st.success("Button works! 🎉")
+    st.dataframe(data_test)
